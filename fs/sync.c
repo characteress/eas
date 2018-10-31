d629d07c3d025ad8f7ae3d9c8be1bba7c66a95f6
characteress <gustavogcm.ferragil@gmail.com>
2018-10-31 19:57:50 -0300

    Disable Fsync for improved disk performance

--- fs/sync.c
+++ fs/sync.c
@@ -17,6 +17,12 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
+bool fsync_enabled = false;
+module_param(fsync_enabled, bool, 0755);
+#ifdef CONFIG_DYNAMIC_FSYNC
+#include <linux/dyn_sync_cntrl.h>
+#endif
+
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
