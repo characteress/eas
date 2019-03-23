2bc41fab37ee9c4f795585de3d0cd2c7298ef356
Gustavo <38637402+characteress@users.noreply.github.com>
2019-03-23 17:47:12 -0300

    force enable dynamic stune boost and disable inputboost

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -34,18 +34,18 @@ struct cpu_sync {
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 
 static struct kthread_work input_boost_work;
-static unsigned int input_boost_enabled = 1;
+static unsigned int input_boost_enabled = 0;
 module_param(input_boost_enabled, uint, 0644);
 
 static unsigned int input_boost_ms = 1;
 module_param(input_boost_ms, uint, 0644);
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+
 static int dynamic_stune_boost;
 module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
 static int boost_slot;
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
@@ -180,13 +180,13 @@ static void do_input_boost_rem(struct work_struct *work)
 		i_sync_info->input_boost_min = 0;
 	}
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+
 	/* Reset dynamic stune boost value to the default value */
 	if (stune_boost_active) {
 		reset_stune_boost("top-app", boost_slot);
 		stune_boost_active = false;
 	}
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
@@ -214,12 +214,12 @@ static void do_input_boost(struct kthread_work *work)
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+
 	/* Set dynamic stune boost value */
 	ret = do_stune_boost("top-app", dynamic_stune_boost, &boost_slot);
 	if (!ret)
 		stune_boost_active = true;
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 
 	schedule_delayed_work(&input_boost_rem, msecs_to_jiffies(input_boost_ms));
 }
@@ -275,10 +275,10 @@ err2:
 
 static void cpuboost_input_disconnect(struct input_handle *handle)
 {
-#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+
 	/* Reset dynamic stune boost value to the default value */
 	reset_stune_boost("top-app", boost_slot);
-#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 
 	input_close_device(handle);
 	input_unregister_handle(handle);
